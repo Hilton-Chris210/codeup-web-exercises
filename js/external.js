0862de2a32bc38d3618e241fc0f5bf97adc0e641
Chris Hilton <hilton.chris210@gmail.com>
2022-11-21 13:40:10 -0600

    Initial push after file transfer

---
+++ js/external.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hello from external JavaScript');
+alert('Welcome To My Website!');
+var favcolor = prompt('What is your favorite color?:');
+let message = "great" + favcolor + " is your favorite color.";
+alert(message)
+console.log(favcolor)
