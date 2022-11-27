d716a4736cae69a630fc196a34b2565fbfea084c
Chris Hilton <hilton.chris210@gmail.com>
2022-11-27 15:45:07 -0600

    final answer complete

--- js/conditionals.js
+++ js/conditionals.js
@@ -160,47 +160,58 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * Test your function by passing it various values and checking for the expected
  * return value.
  */
-let luckyNumber = Math.floor(Math.random() * 6);
-console.log(luckyNumber)
-let randomNumber = Math.floor(Math.random(1) * 100);
-console.log(randomNumber)
+// let luckyNumber = Math.floor(Math.random() * 6);
+// console.log(luckyNumber)
+// let randomNumber = Math.floor(Math.random(1) * 100);
+// console.log(randomNumber)
 // let zeroDiscount = 1
 // let oneDiscount = .90
 // let twoDiscount = .75
 // let threeDiscount = .65
 // let fourDiscount = .50
 // let fiveDiscount = 0
-
-function calculateTotal(luckyNumber, randomNumber)
-
-
-
-if (luckyNumber == 0){
-    alert("Sorry no discount")
-    alert(calculateTotal())
-} else if ( luckyNumber == "1"){
-    alert("Congrats you get a %10 discount")
-    alert(calculateTotal())
-} else if (luckyNumber == "2"){
-    alert("Congrats you get a %25 discount")
-    alert(calculateTotal())
-} else if (luckyNumber == "3"){
-    alert("Congrats you get a %35 discount")
-    alert(calculateTotal())
-} else if (luckyNumber == "4"){
-    alert("Congrats you get a %50 discount")
-    alert(calculateTotal())
-} else if (luckyNumber == "5"){
-    alert("Congrats you get a %100 discount... aka FREE")
-    let fiveDiscount = 0
-    alert(calculateTotal())
-} else {
-    console.log("how the hell did you end up here")
-}
-
-
-
-
+//
+// function calculateTotal(ln, ta){
+//     if (luckyNumber === 0){
+//         alert("Sorry no discount")
+//         alert("your total before discount is $" + randomNumber)
+//         alert("Your total with discount is $" + randomNumber*zeroDiscount)
+//         alert("You saved $" + (randomNumber-(randomNumber*zeroDiscount)))
+//
+//
+//     } else if ( luckyNumber === 1){
+//         alert("Congrats you get a %10 discount")
+//         alert("your total before discount is $" + randomNumber)
+//         alert("Your total with discount is $" + randomNumber*oneDiscount)
+//         alert("You saved $" + (randomNumber-(randomNumber*oneDiscount)))
+//
+//     } else if (luckyNumber === 2){
+//         alert("Congrats you get a %25 discount")
+//         alert("your total before discount is $" + randomNumber)
+//         alert("Your total with discount is $" + randomNumber*twoDiscount)
+//         alert("You saved $" + (randomNumber-(randomNumber*twoDiscount)))
+//
+//     } else if (luckyNumber === 3){
+//         alert("Congrats you get a %35 discount")
+//         alert("your total before discount is $" + randomNumber)
+//         alert("Your total with discount is $" + randomNumber*threeDiscount)
+//         alert("You saved $" + (randomNumber-(randomNumber*threeDiscount)))
+//     } else if (luckyNumber === 4){
+//         alert("Congrats you get a %50 discount")
+//         alert("your total before discount is $" + randomNumber)
+//         alert("Your total with discount is $" + randomNumber*fourDiscount)
+//         alert("You saved $" + (randomNumber-(randomNumber*fourDiscount)))
+//     } else if (luckyNumber === 5){
+//         alert("Congrats you get a %100 discount... aka FREE")
+//         alert("your total before discount is $" + randomNumber)
+//         alert("Your total with discount is $" + randomNumber*fiveDiscount)
+//         alert("You saved $" + (randomNumber-(randomNumber*fiveDiscount)))
+//
+//     }}
+//
+// console.log(calculateTotal(luckyNumber, randomNumber))
+// console.log("The Lucky number is " +luckyNumber)
+// console.log("Total is $" + randomNumber )
 /**
  * TODO:
  * Uncomment the line below to generate a random number between 0 and 5.
--- js/conditionals2.js
+++ js/conditionals2.js
@@ -9,35 +9,44 @@ let threeDiscount = .65
 let fourDiscount = .50
 let fiveDiscount = 0
 
-
-function calculateTotal(luckyNumber, randomNumber){
-return luckyNumber*randomNumber;
-    }
-let totalD = calculateTotal(luckyNumber, randomNumber);
-console.log(totalD)
-
+function calculateTotal(ln, ta){
 if (luckyNumber === 0){
     alert("Sorry no discount")
-    zeroDiscount
+    alert("your total before discount is $" + randomNumber)
+    alert("Your total with discount is $" + randomNumber*zeroDiscount)
+    alert("You saved $" + (randomNumber-(randomNumber*zeroDiscount)))
 
-} else if ( luckyNumber === "1"){
+
+} else if ( luckyNumber === 1){
     alert("Congrats you get a %10 discount")
-    return oneDiscount
+    alert("your total before discount is $" + randomNumber)
+    alert("Your total with discount is $" + randomNumber*oneDiscount)
+    alert("You saved $" + (randomNumber-(randomNumber*oneDiscount)))
 
-} else if (luckyNumber === "2"){
+} else if (luckyNumber === 2){
     alert("Congrats you get a %25 discount")
-    return twoDiscount
+    alert("your total before discount is $" + randomNumber)
+    alert("Your total with discount is $" + randomNumber*twoDiscount)
+    alert("You saved $" + (randomNumber-(randomNumber*twoDiscount)))
 
-} else if (luckyNumber === "3"){
+} else if (luckyNumber === 3){
     alert("Congrats you get a %35 discount")
-    return threeDiscount
-
-} else if (luckyNumber === "4"){
+    alert("your total before discount is $" + randomNumber)
+    alert("Your total with discount is $" + randomNumber*threeDiscount)
+    alert("You saved $" + (randomNumber-(randomNumber*threeDiscount)))
+} else if (luckyNumber === 4){
     alert("Congrats you get a %50 discount")
-    return fourDiscount
-
-} else if (luckyNumber === "5"){
+    alert("your total before discount is $" + randomNumber)
+    alert("Your total with discount is $" + randomNumber*fourDiscount)
+    alert("You saved $" + (randomNumber-(randomNumber*fourDiscount)))
+} else if (luckyNumber === 5){
     alert("Congrats you get a %100 discount... aka FREE")
-    return fiveDiscount
+    alert("your total before discount is $" + randomNumber)
+    alert("Your total with discount is $" + randomNumber*fiveDiscount)
+    alert("You saved $" + (randomNumber-(randomNumber*fiveDiscount)))
 
-}
+}}
+
+console.log(calculateTotal(luckyNumber, randomNumber))
+console.log("The Lucky number is " +luckyNumber)
+console.log("Total is $" + randomNumber )
