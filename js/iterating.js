455354fc17732afaa0bfda2b4d4bc10c3245d341
Chris Hilton <hilton.chris210@gmail.com>
2022-12-02 11:35:41 -0600

    question 4 complete

---
+++ js/iterating.js
@@ -0,0 +1,57 @@
+// What is an array? Element? Index? An array is a data structure that holds an ordered list of items. An item in an array are called elements and they are seperated by commas. arrays are xero indexed which means that the first element in an array is xero index.
+//     How would you access the first element in an array?
+//     var numbers = [1, 2, 3, 4];
+//          console.log(numbers[0]); // 1
+//     What happens when you access an element at an index that has not been assigned a value?
+// var numbers = [1, 2, 3, 4];
+// console.log(numbers[5]); // undefined
+//     What are the differences between a for loop and forEach loop? Why use one over the other?
+// The forEach method on an array takes another function as a parameter and doesn't have a return value. The function passed to forEach takes in up to 3 parameters that will provide access to the array element, the corresponding index, and the array itself. We can refer to the function passed to .forEach as a callback function. The callback function will be called for every item in the array.
+
+(function(){
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+
+})();
