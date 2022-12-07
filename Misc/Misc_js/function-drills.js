519b9f267584f42219ced2257392a9b673d001cd
Chris Hilton <hilton.chris210@gmail.com>
2022-12-07 13:59:37 -0600

    corrected trevor inputs

--- Misc/Misc_js/function-drills.js
+++ Misc/Misc_js/function-drills.js
@@ -1,22 +1,22 @@
-function isEven (userInput) {
-    return (userInput % 2 === 0);
-}
-
-function isOdd (userInput){
-    return (userInput % 2 !== 0)
-}
-
-function isPositive(userInput) {
-    return (Math.sign(parseFloat(userInput)) === 1);
-}
-
-function isMultipleOf (target, n) {
-    if (target % n === 0)
-        return "yes"
-} else {
-    return "no"
-}
-function degToRadian (userInput){
-    return (userInput × Math.PI/180)
-)
-}
+// function isEven (userInput) {
+//     return (userInput % 2 === 0);
+// }
+//
+// function isOdd (userInput){
+//     return (userInput % 2 !== 0)
+// }
+//
+// function isPositive(userInput) {
+//     return (Math.sign(parseFloat(userInput)) === 1);
+// }
+//
+// function isMultipleOf (target, n) {
+//     if (target % n === 0)
+//         return "yes"
+// } else {
+//     return "no"
+// }
+// function degToRadian (userInput){
+//     return (userInput × Math.PI/180)
+// )
+// }
