94c3aaba1e7118f37cabe3905a89badc9e2d629a
Chris Hilton <hilton.chris210@gmail.com>
2022-12-07 14:50:35 -0600

    corrected trevor inputs

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,38 +1,19 @@
-let newNum = prompt("Please enter an ODD NUMBER between 1-50")
-    if (newNum === (isEvenOdd(newNum)=== "even")) {
-        prompt("Please enter an ODD NUMBER between 1-50");
-    } else if (newNum < 1) {
-        prompt("Please enter an ODD NUMBER between 1-50");
-    } else if (newNum > 50) {
-        prompt("Please enter an ODD NUMBER between 1-50");
-    }else {
-        console.log('For loop to show the stuff');
+
+// 3. Use a loop and the continue statement to output all the
+// odd numbers between 1 and 50, except for the number the
+// user entered.
+
+while(true){
+    let userNum = prompt("Enter an odd number between 1 and 50");
+    userNum = parseFloat(userNum);
+    if (userNum % 2 === 1 && userNum <= 50 && userNum >= 1) {
+        for (let i = 1; i <= 50; i+=2) {
+            if (i === userNum) {
+                console.log(`Yikes! Skipping number ${i}`);
+                continue;
+            }
+            console.log(`Here is an odd number: ${i}`)
+        }
+        break;
     }
-        //     for (let i = 1; i <= 50; i++){
-        //         let result =;
-        //             console.log()
-        //     }
-        // }
-
-
-        function isEvenOdd(input) {
-            if (input % 2 === 0) {
-                return "even"
-            } else {
-                return "odd"
-            }}
-
-
-    //     'use strict';
-    //
-    //     for (let i = 1; i < 50; i++) {
-    //
-    //         if (i % 2 === 0) {
-    //             // numb is even
-    //             // skip the rest of the loop and continue with the next iteration
-    //             continue;
-    //         }
-    //
-    //         console.log('Here is a lovely odd number: ' + i);
-    //     }
-    // }
+}
