8e632a267d5a52f62b5695b965a84a3a40b75f81
Chris Hilton <hilton.chris210@gmail.com>
2023-01-24 15:02:00 -0600

    afternoon push

---
+++ js/fromscratch.js
@@ -0,0 +1,19 @@
+let cars = [
+    {make: 'jeep',
+        model: 'wranlger',
+        year: '2016'},
+    {make: 'honda',
+        model: 'accord',
+        year: '2016'},
+    {make: 'ford',
+        model: 'f150',
+        year: '2016'}
+]
+
+function createArray(arr){
+    let makes = []
+    for(let i = 0; i < arr.length; i++){
+    }
+    return makes.push(cars[i].make)
+}
+
